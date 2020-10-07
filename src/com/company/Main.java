e6fb998c3f0191491f104347573f421632ba7500
katseliv <katseliv@yandex.ru>
2020-10-07 09:22:06 +0300

    1.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Service service = new Service();
-        service.start(new Fool(), 3, 52);
+        service.start(new Fool(), 3, 36);
     }
 }
--- src/com/company/Service.java
+++ src/com/company/Service.java
@@ -19,6 +19,8 @@ public class Service { //методы для обработки данных
     public void start(Fool fool, int amountOfPlayers, int amountOfCards) {
         initialization(fool, amountOfPlayers, amountOfCards);
         distributeCards(fool);
+
+        giveCards(fool);
     }
 
     public void initialization(Fool fool, int amountOfPlayers, int amountOfCards) { //создание всего
@@ -26,16 +28,16 @@ public class Service { //методы для обработки данных
         List<Card> cards = initializationCards(fool, amountOfCards);
         shuffleCards(cards);
         chooseTrump(cards);
+
     }
 
     private List<Card> initializationCards(Fool fool, int amount) {
         List<Card> cards = fool.getCards();
-
         String[] numberOfCards = NumberOfCards.NUMBER_OF_CARDS;
         char[] cardSuit = CardSuit.CARD_SUIT;
 
         if (amount == 36) {
-            for (int i = 5; i < numberOfCards.length; i++) {
+            for (int i = 4; i < numberOfCards.length; i++) {
                 for (char suit : cardSuit) {
                     cards.add(new Card(numberOfCards[i], suit));
                 }
@@ -49,24 +51,31 @@ public class Service { //методы для обработки данных
         } else {
             System.out.println(ANSI_PURPLE + "Такое кол-во карт недопустимо!!!" + ANSI_RESET);
         }
-
         return cards;
     }
 
     private void initializationPlayers(Fool fool, int amount) {
         ArrayDeque<Player> players = fool.getPlayers();
-
         for (int i = 1; i <= amount; i++) {
             players.add(new Player(i));
         }
+        System.out.println("Инициализация игроков");
+        for(Player player : players){
+            System.out.println(player);
+        }
+
     }
 
     private void shuffleCards(List<Card> cards) {
         System.out.println(ANSI_RESET + ANSI_BLUE + "Генерация колоды:" + ANSI_BLACK);
         Collections.shuffle(cards);
+
+        int i = 0;
         for (Card card : cards) {
             System.out.println(card);
+            i++;
         }
+        System.out.println("Кол-во карт " + i);
     }
 
     void distributeCards(Fool fool) {
@@ -76,10 +85,9 @@ public class Service { //методы для обработки данных
 
         for (Player player : players) {
             Set<Card> cardsOfPlayer = new HashSet<>();
-            for (int i = 1; i <= 6; i++) {
-                int j = (int) (Math.random() * cards.size());
-                cardsOfPlayer.add(cards.get(j));
-                cards.remove(j);
+            for (int i = 0; i < 3; i++) { // 6 must be
+                cardsOfPlayer.add(cards.get(0));
+                cards.remove(0);
             }
             ratio.put(player, cardsOfPlayer);
         }
@@ -92,17 +100,22 @@ public class Service { //методы для обработки данных
                 System.out.println(card);
             }
         }
-    }
 
+        System.out.println();
+        int i = 0;
+        for(Card card : cards){
+            System.out.println(card);
+            i++;
+        }
+        System.out.println("Кол-во карт " + i);
+    }
 
     void chooseTrump(List<Card> cards) {
         System.out.println();
-        int randomNumber = (int) (Math.random() * cards.size());
-        Card trump = cards.get(randomNumber);
+        int number = cards.size() - 1;
+        Card trump = cards.get(number);
         System.out.println(ANSI_RESET + "Trump -> " + ANSI_RED + trump);
-        cards.remove(randomNumber);
-        cards.add(cards.size(), trump);
-
+        System.out.println();
         for (Card card : cards) {
             System.out.println(card);
         }
@@ -118,12 +131,41 @@ public class Service { //методы для обработки данных
 
     }
 
-    void tossUp(){
+    void tossUp() {
 
     }
 
-    void giveCards() {
+    void giveCards(Fool fool) {
+        System.out.println();
+        System.out.println("Добавили карты: ");
+        Map<Player, Set<Card>> ratio = fool.getRatio();
+        List<Card> cards = fool.getCards();
 
+        for (Map.Entry<Player, Set<Card>> playerSetEntry : ratio.entrySet()) {
+            int size = 6 - playerSetEntry.getValue().size();
+            if (size > 0) {
+
+                for (int i = 0; i < size; i++) {
+                    playerSetEntry.getValue().add(cards.get(0));
+                    cards.remove(0);
+                }
+            }
+        }
+
+        for (Map.Entry<Player, Set<Card>> playerSetEntry : ratio.entrySet()) {
+            System.out.println(ANSI_RESET + ANSI_GREEN + playerSetEntry.getKey() + ANSI_BLACK + " -> ");
+            for (Card card : playerSetEntry.getValue()) {
+                System.out.println(card);
+            }
+        }
+
+        System.out.println();
+        int i = 0;
+        for(Card card : cards){
+            System.out.println(card);
+            i++;
+        }
+        System.out.println("Кол-во карт " + i);
     }
 
 }
